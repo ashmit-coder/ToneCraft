037f31cac376cb261d896dc9b60c3e8a8ba563b4
ashmit-coder <ashmitjagtap2209@gmail.com>
2024-01-28 14:08:01 +0530

    test3

--- dummy.py
+++ dummy.py
@@ -5,7 +5,7 @@ api_url = "https://tonecraft-ml.onrender.com/upload_pdf/"
 files = {'file': open(sys.argv[1], 'rb')}
 # files = {'file': open(pdf_file_path, 'rb')}
 response = requests.post(api_url, files=files)
-
+print("python running")
 if response.status_code == 200:
     with open(f"combined_audio.mp3", "wb") as file:
         file.write(response.content)
